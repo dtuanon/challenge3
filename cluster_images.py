1f931eee2736d59429387fde251546c7543744f3
dtuanon <s135150@student.dtu.dk>
2017-12-05 14:56:43 +0100

    modify main and add gmm

--- cluster_images.py
+++ cluster_images.py
@@ -1,4 +1,5 @@
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture as GMM
 
 def cluster_videos_kmeans(X, names, n_clusters = 970):
 	kmeans			= KMeans(n_clusters=n_clusters, n_jobs = -1)
@@ -8,4 +9,16 @@ def cluster_videos_kmeans(X, names, n_clusters = 970):
 	for cluster_idx, name in zip(belonging_index, names):
 		clusters[cluster_idx].add(name)
 	
+	return clusters
+	
+def cluster_videos_gmm(X, names, n_clusters = 970):
+	cov_type	= "tied"
+	n_init		= 3
+	
+	gmm 		= GMM( n_components = n_clusters, covariance_type = cov_type, n_init = n_init )
+	gmm.fit(X)
+	belonging_index	= gmm.predict(X)
+	clusters		= [set() for _ in range(n_clusters)]
+	for cluster_idx, name in zip(belonging_index, names):
+		clusters[cluster_idx].add(name)
 	return clusters
